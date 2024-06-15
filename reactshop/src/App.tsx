fbf434633b614c80bf0918b9004a3b9bbd9b78b6
Gisele <88586474+giseletoledo@users.noreply.github.com>
2024-06-15 02:03:58 +0000

    criando componente productslist

--- reactshop/src/App.tsx
+++ reactshop/src/App.tsx
@@ -3,12 +3,13 @@ import styled from "styled-components"
 import { GlobalStyles } from './styles/GlobalStyles';
 import { Header } from './components/Header/Header';
 import { ProductCard } from './components/ProductCard/ProductCard';
+import { ProductsList } from './components/ProductsList/ProductsList';
 
 function App() {
   return (
     <>
     <Header/>
-    <ProductCard />
+    <ProductsList />
    <GlobalStyles />
    </>
   );
